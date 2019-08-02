2d4d0665f9c8d673fea3c4afcb29513890d9f862
Kryna <1035110219@qq.com>
2019-08-02 10:18:18 +0800

    职位查询接口修改@廖莉娜 2019/8/2

--- hand-crm/src/main/java/com/hand/position/controller/PositionController.java
+++ hand-crm/src/main/java/com/hand/position/controller/PositionController.java
@@ -26,10 +26,31 @@ public class PositionController {
     @ApiOperation(value="查询职位")
     @ApiImplicitParams({
             @ApiImplicitParam(paramType="query", name="currentPage", value="当前页", dataType="int"),
-            @ApiImplicitParam(paramType="query", name="pageSize", value="页大小", dataType="int")
+            @ApiImplicitParam(paramType="query", name="pageSize", value="页大小", dataType="int"),
+            @ApiImplicitParam(paramType="query", name="code", value="职位编码", dataType="String"),
+            @ApiImplicitParam(paramType="query", name="name", value="名称", dataType="String"),
+            @ApiImplicitParam(paramType="query", name="desc", value="描述", dataType="String"),
+            @ApiImplicitParam(paramType="query", name="level", value="职级", dataType="String"),
+            @ApiImplicitParam(paramType="query", name="type", value="类型", dataType="String"),
+            @ApiImplicitParam(paramType="query", name="orgCode", value="组织编码", dataType="String"),
+            @ApiImplicitParam(paramType="query", name="priEmpCode", value="主要员工编码", dataType="String"),
+            @ApiImplicitParam(paramType="query", name="status", value="是否有效", dataType="String")
     })
-    @PostMapping("/getPosInfo")
-    public ServiceData getPosInfo(@RequestBody(required = false) PositionVO positionVO, int currentPage, int pageSize){
+    @GetMapping("/getPosInfo")
+    public ServiceData getPosInfo(int currentPage, int pageSize,
+                                  @RequestParam(value = "code",required = false) String code, @RequestParam(value = "name",required = false) String name,
+                                  @RequestParam(value = "desc",required = false) String desc, @RequestParam(value = "level",required = false) String level,
+                                  @RequestParam(value = "type",required = false) String type, @RequestParam(value = "orgCode",required = false) String orgCode,
+                                  @RequestParam(value = "priEmpCode",required = false) String priEmpCode, @RequestParam(value = "status",required = false) String status){
+        PositionVO positionVO = new PositionVO();
+        positionVO.setCode(code);
+        positionVO.setName(name);
+        positionVO.setDesc(desc);
+        positionVO.setLevel(level);
+        positionVO.setType(type);
+        positionVO.setOrgCode(orgCode);
+        positionVO.setPriEmpCode(priEmpCode);
+        positionVO.setStatus(status);
         PageQuery<PositionVO> pageQuery = new PageQuery<PositionVO>(positionVO, currentPage, pageSize);
         return  ServiceData.success(positionService.getPosition(pageQuery), pageQuery.getMapOfPageQuery());
     }
