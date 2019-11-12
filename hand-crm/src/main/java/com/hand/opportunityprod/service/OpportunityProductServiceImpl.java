f6445fa097acca689dc0b48da1074689fce82b0c
LDH <dinghuai.liu@hand-china.com>
2019-11-12 09:40:46 +0800

    model：商机产品 created by:LIUDH created time:2019/11/12

--- hand-crm/src/main/java/com/hand/opportunityprod/service/OpportunityProductServiceImpl.java
+++ hand-crm/src/main/java/com/hand/opportunityprod/service/OpportunityProductServiceImpl.java
@@ -18,7 +18,7 @@ public class OpportunityProductServiceImpl implements OpportunityProductService 
     @Override
     public ResultDTO addOpportunityProd(OpportunityProductVO opportunityProductVO) {
         if (!StringUtil.isEmpty(opportunityProductVO.getOptyCode())&& !StringUtil.isEmpty(opportunityProductVO.getUpdatedBy())
-                && !StringUtil.isEmpty(opportunityProductVO.getProductCode()) && !StringUtil.isEmpty(opportunityProductVO.getPrimaryFlag())
+                && !StringUtil.isEmpty(opportunityProductVO.getPrimaryFlag())
                 && (opportunityProductVO.getExpectSignTime()!=null) && (opportunityProductVO.getAmount()!=null))
         {
             String code = StringUtil.getCode();
